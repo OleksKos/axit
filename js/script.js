28e2a810665a7ed674e20928cf64aa5dba19e48c
gidroadmin <maindmn@gmail.com>
2020-11-13 17:32:27 +0200

    autoslider

--- js/script.js
+++ js/script.js
@@ -308,7 +308,6 @@ function scroll() {
    startPosition = scrolled;
 }
 
-
 'use strict';
 var multiItemSlider = (function () {
    return function (selector, config) {
@@ -320,50 +319,89 @@ var multiItemSlider = (function () {
          _sliderControlLeft = _mainElement.querySelector('.slider__control_left'), // кнопка "LEFT"
          _sliderControlRight = _mainElement.querySelector('.slider__control_right'), // кнопка "RIGHT"
          _wrapperWidth = parseFloat(getComputedStyle(_sliderWrapper).width), // ширина обёртки
-         _itemWidth = parseFloat(getComputedStyle(_sliderItems[0]).width), // ширина одного элемента
+         _itemWidth = parseFloat(getComputedStyle(_sliderItems[0]).width), // ширина одного элемента    
          _positionLeftItem = 0, // позиция левого активного элемента
          _transform = 0, // значение транфсофрмации .slider_wrapper
          _step = _itemWidth / _wrapperWidth * 100, // величина шага (для трансформации)
-         _items = []; // массив элементов
-      var _startX = 0;
+         _items = [], // массив элементов
+         _interval = 0,
+         _config = {
+            isCycling: false, // автоматическая смена слайдов
+            direction: 'right', // направление смены слайдов
+            interval: 5000, // интервал между автоматической сменой слайдов
+            pause: true // устанавливать ли паузу при поднесении курсора к слайдеру
+         };
+
+      for (var key in config) {
+         if (key in _config) {
+            _config[key] = config[key];
+         }
+      }
+
       // наполнение массива _items
       _sliderItems.forEach(function (item, index) {
          _items.push({ item: item, position: index, transform: 0 });
       });
 
       var position = {
-         getMin: 0,
-         getMax: _items.length - 1,
+         getItemMin: function () {
+            var indexItem = 0;
+            _items.forEach(function (item, index) {
+               if (item.position < _items[indexItem].position) {
+                  indexItem = index;
+               }
+            });
+            return indexItem;
+         },
+         getItemMax: function () {
+            var indexItem = 0;
+            _items.forEach(function (item, index) {
+               if (item.position > _items[indexItem].position) {
+                  indexItem = index;
+               }
+            });
+            return indexItem;
+         },
+         getMin: function () {
+            return _items[position.getItemMin()].position;
+         },
+         getMax: function () {
+            return _items[position.getItemMax()].position;
+         }
       }
 
       var _transformItem = function (direction) {
+         var nextItem;
          if (direction === 'right') {
-            if ((_positionLeftItem + _wrapperWidth / _itemWidth - 1) >= position.getMax) {
-               return;
-            }
-            if (!_sliderControlLeft.classList.contains('slider__control_show')) {
-               _sliderControlLeft.classList.add('slider__control_show');
-            }
-            if (_sliderControlRight.classList.contains('slider__control_show') && (_positionLeftItem + _wrapperWidth / _itemWidth) >= position.getMax) {
-               _sliderControlRight.classList.remove('slider__control_show');
-            }
             _positionLeftItem++;
+            if ((_positionLeftItem + _wrapperWidth / _itemWidth - 1) > position.getMax()) {
+               nextItem = position.getItemMin();
+               _items[nextItem].position = position.getMax() + 1;
+               _items[nextItem].transform += _items.length * 100;
+               _items[nextItem].item.style.transform = 'translateX(' + _items[nextItem].transform + '%)';
+            }
             _transform -= _step;
          }
          if (direction === 'left') {
-            if (_positionLeftItem <= position.getMin) {
-               return;
-            }
-            if (!_sliderControlRight.classList.contains('slider__control_show')) {
-               _sliderControlRight.classList.add('slider__control_show');
-            }
-            if (_sliderControlLeft.classList.contains('slider__control_show') && _positionLeftItem - 1 <= position.getMin) {
-               _sliderControlLeft.classList.remove('slider__control_show');
-            }
             _positionLeftItem--;
+            if (_positionLeftItem < position.getMin()) {
+               nextItem = position.getItemMax();
+               _items[nextItem].position = position.getMin() - 1;
+               _items[nextItem].transform -= _items.length * 100;
+               _items[nextItem].item.style.transform = 'translateX(' + _items[nextItem].transform + '%)';
+            }
             _transform += _step;
          }
          _sliderWrapper.style.transform = 'translateX(' + _transform + '%)';
+      }
+
+      var _cycle = function (direction) {
+         if (!_config.isCycling) {
+            return;
+         }
+         _interval = setInterval(function () {
+            _transformItem(direction);
+         }, _config.interval);
       }
 
       // обработчик события click для кнопок "назад" и "вперед"
@@ -372,8 +410,26 @@ var multiItemSlider = (function () {
             e.preventDefault();
             var direction = e.target.classList.contains('slider__control_right') ? 'right' : 'left';
             _transformItem(direction);
+            clearInterval(_interval);
+            _cycle(_config.direction);
          }
       };
+
+      var _setUpListeners = function () {
+         // добавление к кнопкам "назад" и "вперед" обрботчика _controlClick для событя click
+         _sliderControls.forEach(function (item) {
+            item.addEventListener('click', _controlClick);
+         });
+         if (_config.pause && _config.isCycling) {
+            _mainElement.addEventListener('mouseenter', function () {
+               clearInterval(_interval);
+            });
+            _mainElement.addEventListener('mouseleave', function () {
+               clearInterval(_interval);
+               _cycle(_config.direction);
+            });
+         }
+      }
 
       var _setUpListeners = function () {
          // добавление к кнопкам "назад" и "вперед" обрботчика _controlClick для событя click
@@ -399,6 +455,7 @@ var multiItemSlider = (function () {
 
       // инициализация
       _setUpListeners();
+      _cycle(_config.direction);
 
       return {
          right: function () { // метод right
@@ -406,10 +463,21 @@ var multiItemSlider = (function () {
          },
          left: function () { // метод left
             _transformItem('left');
+         },
+         stop: function () { // метод stop
+            _config.isCycling = false;
+            clearInterval(_interval);
+         },
+         cycle: function () { // метод cycle 
+            _config.isCycling = true;
+            clearInterval(_interval);
+            _cycle();
          }
       }
 
    }
 }());
 
-var slider = multiItemSlider('.slider');
+var slider = multiItemSlider('.slider', {
+   isCycling: true
+})
